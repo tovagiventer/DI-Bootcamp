5b8600ca3e05a92c36da53cdd6dbd42b98a027f3
tovagiventer <tovagiventer@gmail.com>
2025-11-10 13:45:20 +0200

    added instructions to submit daily challenge

--- DI-Bootcamp-week1/Week1/DailyChallenge/day1DailyChallenge.py
+++ DI-Bootcamp-week1/Week1/DailyChallenge/day1DailyChallenge.py
@@ -1,4 +1,32 @@
-import random
+# Instructions:
+# 1. Ask for User Input:
+
+# The string must be exactly 10 characters long.
+# 2. Check the Length of the String:
+
+# If the string is less than 10 characters, print: "String not long enough."
+# If the string is more than 10 characters, print: "String too long."
+# If the string is exactly 10 characters, print: "Perfect string" and proceed to the next steps.
+# 3. Print the First and Last Characters:
+
+# Once the string is validated, print the first and last characters.
+# 4. Build the String Character by Character:
+
+# Using a for loop, construct and print the string character by character. Start with the first character, then the first two characters, and so on, until the entire string is printed.
+# Hint: You can create a loop that goes through the string, adding one character at a time, and print it progressively.
+
+# Example:
+
+# Alt text
+
+# 5. Bonus: Jumble the String (Optional)
+
+# As a bonus, try shuffling the characters in the string and print the newly jumbled string.
+# Hint: You can use the random.shuffle function to shuffle a list of characters.
+
+
+
+#import random
 
 rope = input('Write a 10-character string: ')
 if len(rope) < 10:
